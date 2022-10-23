cd94e2fd42bf1eff19e695d89549f97f5ff39494
Guillermo <guillef33@gmail.com>
2022-10-23 16:05:39 -0300

    nav home and header

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,18 +1,50 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Menu, MenuItem } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
-import '../Header/Nav.css'
+import "../Header/Nav.css";
+
+import Logo from "../../Assets/logo_header.png";
 
 function Nav() {
 
-  return (
-    <ul className="flex-container">
-      <Typography variant="boo1"><Link to="/">Contador App</Link></Typography>
-      <Button><Link to="/auth/login">Ingresar</Link></Button>
-    <Button><Link to="/auth/register">Registrate</Link></Button>
+ const [anchorEl, setAnchorEl] = React.useState(null);
+ const open = Boolean(anchorEl);
+ const handleClick = (event) => {
+   setAnchorEl(event.currentTarget);
+ };
+ const handleClose = () => {
+   setAnchorEl(null);
+ };
 
-    </ul>
+  return (
+    <>
+      {/* <ul className="flex-container">
+        <Link component={Link}>
+          <img src={Logo} alt="logos" style={{ height: "30px" }} />
+        </Link>
+
+        <Button>
+          <Link to="/auth/login">Ingresar</Link>
+        </Button>
+        <Button>
+          <Link to="/auth/register">Registrate</Link>
+        </Button>
+      </ul> */}
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 }
 
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React from "react";
 
 import { Link } from "react-router-dom";
-import LoginPage from './LoginPage';
+import LoginPage from "./LoginPage";
 
-import { Container, Box, Button, Typography } from '@mui/material';
+import { Container, Box, Button, Typography } from "@mui/material";
 
-import ContadorPublico from '../Assets/contador-publico.jpg';
-import './Home.css'
+import ContadorPublico from "../Assets/header-img-1.png";
+import "./Home.css";
 
-import Nav from '../Components/Nav/Nav'
+import Nav from "../Components/Nav/Nav";
+import MenuMui from "../Components/Header/MenuMui";
 
 function Home() {
+  const styles = {
+    containerContainer: {
+      padding: 0,
+    },
+    boxContainer: {
+      height: "90vh",
+      backgroundImage: `url(${ContadorPublico})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    },
+
+    wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "start",
+      justifyContent: "center",
+      height: "70vh",
+    },
+  };
+
   return (
-    <Container className='home-container'>
-      <Nav />
-      <Box>
-        <img src={ContadorPublico} alt="contador-publico-imagen" />
+    <Container
+      className="home-container"
+      maxWidth={false}
+      style={styles.containerContainer}
+    >
+      <MenuMui />
+      <Box style={styles.boxContainer}>
+        <Box style={styles.wrapper}>
+          <Typography variant="h2">Sistema de Gestión</Typography>
+          <Typography variant="book1">
+            Con nuestro software ERP en la nube tu empresa crece
+          </Typography>
+          <Button>
+            <Link to="/auth/register" className="main-button">
+              Prueba gratis por 10 dias
+            </Link>
+          </Button>
+        </Box>
+        {/* <img src={ContadorPublico} alt="contador-publico-imagen" /> */}
       </Box>
       {/* <Box>
         <Button>
@@ -26,7 +62,7 @@ function Home() {
       </Button>
       </Box> */}
     </Container>
-  )
+  );
 }
 
-export default Home
+export default Home;
